fbd4f1847ce85d991ce631b6dc5e64fa0fac538e
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-04-11 19:10:36 +0200

    Crc computation from google's hash to crc numbers

--- swh/fetcher/google/hashutil.py
+++ swh/fetcher/google/hashutil.py
@@ -3,16 +3,15 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+import base64
 import crcmod.predefined
 import hashlib
-import sys
-import base64
+import os
+import struct
 
 from swh.model import hashutil
 
 
-# crc32_func = crcmod.mkCrcFun(0x104c11db7, initCrc=0x00000000, xorOut=0xffffffff)
-# crc32c_func = crcmod.mkCrcFun(0x11edc6f41, initCrc=0x00000000, xorOut=0xffffffff)
 crc32c_func = crcmod.predefined.mkCrcFun('crc-32c')
 
 
@@ -24,12 +23,18 @@ def crc32c_hash(fobj):
             break
         h = crc32c_func(chunk, h)
 
-    # convert checksum to hex bytes
-    return h.to_bytes((h.bit_length() // 8) + 1, byteorder='big')
+    return h
 
 
+def crc32c_from_b64(crc32c_b64):
+    return struct.unpack('>L', base64.b64decode(crc32c_b64))[0]
 
-def md5(fobj):
+
+def md5_from_b64(md5_b64):
+    return base64.b64decode(md5_b64)
+
+
+def md5_hash(fobj):
     h = hashlib.md5()
     while True:
         chunk = fobj.read(hashutil.HASH_BLOCK_SIZE)
@@ -41,23 +46,28 @@ def md5(fobj):
 
 
 if __name__ == '__main__':
-    # filename = sys.argv[1]
-    filename = 'hg4j.zip'
-    # meta: https://www.googleapis.com/storage/v1/b/google-code-archive-source/o/v2%2Fcode.google.com%2Fhg4j%2Fsource-archive.zip
-    # file: https://www.googleapis.com/storage/v1/b/google-code-archive-source/o/v2%2Fcode.google.com%2Fhg4j%2Fsource-archive.zip?alt=media
+    import sys
+    filename = sys.argv[1]
+    filename_meta = filename + '.json'
+    import json
 
-    expected_md5 = base64.b64decode(b'FaIRjuSDe4v51H1+sRuggQ==')
-    expected_crc32c = base64.decodebytes(b'PNKIqA==')
+    with open(filename_meta, 'r') as f:
+        data = json.loads(f.read())
+
+    expected_md5 = md5_from_b64(data['md5Hash'])
+    expected_crc32c = crc32c_from_b64(data['crc32c'])
 
     with open(filename, 'rb') as f:
-        md5_hash = md5(f)
-        if md5_hash != expected_md5:
-            print('Expected md5: %s - Computed md5: %s' % (
-                expected_md5, md5_hash))
+        md5_h = md5_hash(f)
+        print('Expected md5: %s - Computed md5: %s' % (
+            expected_md5, md5_h))
+        if md5_h != expected_md5:
+            print('md5 does not match')
 
         f.seek(0)
 
-        crc32c_hash_signed = crc32c_hash(f)
-        if crc32c_hash_signed != expected_crc32c:
-            print('expected_crc32c: %s - Computed crc32c: %s' % (
-                expected_crc32c, crc32c_hash_signed))
+        crc32c_h = crc32c_hash(f)
+        print('expected_crc32c: %s - Computed crc32c: %s' % (
+            expected_crc32c, crc32c_h))
+        if expected_crc32c != crc32c_h:
+            print('crc32c does not match.')
