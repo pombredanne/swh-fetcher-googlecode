f8e55b2b2c26da3e4b263dc06625729da3392624
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-04-12 12:17:22 +0200

    Refactor - Split in functions and remove corrupted downloaded file

--- swh/fetcher/googlecode/loader.py
+++ swh/fetcher/googlecode/loader.py
@@ -53,6 +53,75 @@ class SWHGoogleFetcher(config.SWHConfig):
         l = logging.getLogger('requests.packages.urllib3.connectionpool')
         l.setLevel(logging.WARN)
 
+    def retrieve_source_meta(self, url_meta, filepath_meta):
+        if os.path.exists(filepath_meta):
+            import json
+            with open(filepath_meta, 'r') as f:
+                meta = json.loads(f.read())
+        else:
+            meta = {}
+            try:
+                r = requests.get(url_meta)
+            except Exception as e:
+                msg = 'Problem when fetching metadata %s.' % url_meta
+                self.log.error(msg)
+                raise ValueError(msg, e)
+            else:
+                meta = r.json()
+
+                with open(filepath_meta, 'w') as f:
+                    f.write(r.text)
+
+        return meta
+
+    def retrieve_source(self, url, filepath):
+        if not os.path.exists(filepath):
+            self.log.debug('Fetching %s\' raw data.' % url)
+            try:
+                r = requests.get(url, stream=True)
+            except Exception as e:
+                msg = 'Problem when fetching file %s.' % url
+                self.log.error(msg)
+                raise ValueError(msg, e)
+            else:
+                with open(filepath, 'wb') as f:
+                    for chunk in r.iter_content(hashutil.HASH_BLOCK_SIZE):
+                        f.write(chunk)
+
+    def check_source(self, meta, filepath):
+        expected = {
+            'crc32c': crc32c_from_b64(meta['crc32c']),
+            'md5': md5_from_b64(meta['md5Hash']),
+            'size': int(meta['size'])
+        }
+
+        actual_size = os.path.getsize(filepath)
+        if actual_size != expected['size']:
+            msg = 'Bad size. Expected: %s. Got: %s' % (
+                expected['size'], actual_size)
+            self.log.error(msg)
+            raise ValueError(msg)
+
+        self.log.debug('Checking %s\' raw data checksums and size.' %
+                       filepath)
+        # Last, check the metadata are ok
+        with open(filepath, 'rb') as f:
+            md5_h = md5_hash(f)
+            if md5_h != expected['md5']:
+                msg = 'Bad md5 signature. Expected: %s. Got: %s' % (
+                    expected['md5'], md5_h)
+                self.log.error(msg)
+                raise ValueError(msg)
+
+            f.seek(0)
+
+            crc32c_h = crc32c_hash(f)
+            if expected['crc32c'] != crc32c_h:
+                msg = 'Bad crc32c signature. Expected: %s. Got: %s' % (
+                    expected['crc32c'], crc32c_h)
+                self.log.error(msg)
+                raise ValueError(msg)
+
     def process(self, archive_gs, destination_rootpath):
         self.log.info('Fetch %s\'s metadata' % archive_gs)
 
@@ -72,77 +141,20 @@ class SWHGoogleFetcher(config.SWHConfig):
         filepath = os.path.join(parent_dir, filename)
         filepath_meta = os.path.join(parent_dir, filename_meta)
 
-        if os.path.exists(filepath_meta):
-            import json
-            with open(filepath_meta, 'r') as f:
-                meta = json.loads(f.read())
-        else:
-            meta = {}
-            try:
-                r = requests.get(url_meta)
-            except Exception as e:
-                msg = 'Problem when fetching metadata %s.' % url_meta
-                self.log.error(msg)
-                raise ValueError(msg, e)
-            else:
-                meta = r.json()
-                self.log.info('Writing %s\'s metadata' % archive_gs)
-
-                print(meta['size'], filename_meta)
-
-                with open(filepath_meta, 'w') as f:
-                    f.write(r.text)
+        meta = self.retrieve_source_meta(url_meta, filepath_meta)
+        if not meta:
+            raise ValueError('Fail to download metadata, stop.')
 
         # Second retrieve the actual content
-        if not meta:
-            raise 'Fail to download metadata, stop.'
+        self.retrieve_source(meta['mediaLink'], filepath)
 
-        expected = {
-            'crc32c': crc32c_from_b64(meta['crc32c']),
-            'md5': md5_from_b64(meta['md5Hash']),
-            'size': int(meta['size'])
-        }
-
-        self.log.debug('Fetching %s\' raw data.' % archive_gs)
-
-        if not os.path.exists(filepath):
-            try:
-                r = requests.get(meta['mediaLink'], stream=True)
-            except Exception as e:
-                msg = 'Problem when fetching metadata %s.' % url_meta
-                self.log.error(msg)
-                raise ValueError(msg, e)
-            else:
-                with open(filepath, 'wb') as f:
-                    for chunk in r.iter_content(hashutil.HASH_BLOCK_SIZE):
-                        f.write(chunk)
-
-        actual_size = os.path.getsize(filepath)
-        if actual_size != expected['size']:
-            msg = 'Bad size. Expected: %s. Got: %s' % (
-                expected['size'], actual_size)
-            self.log.error(msg)
-            raise ValueError(msg)
-
-        self.log.debug('Checking %s\' raw data checksums and size.' %
-                           archive_gs)
-        # Last, check the metadata are ok
-
-        with open(filepath, 'rb') as f:
-            md5_h = md5_hash(f)
-            if md5_h != expected['md5']:
-                msg = 'Bad md5 signature. Expected: %s. Got: %s' % (
-                    expected['md5'], md5_h)
-                self.log.error(msg)
-                raise ValueError(msg)
-
-            f.seek(0)
-
-            crc32c_h = crc32c_hash(f)
-            if expected['crc32c'] != crc32c_h:
-                msg = 'Bad crc32c signature. Expected: %s. Got: %s' % (
-                    expected['crc32c'], crc32c_h)
-                self.log.error(msg)
-                raise ValueError(msg)
-
-        self.log.info('Archive %s fetched.' % archive_gs)
+        # Third - Check the retrieved source
+        try:
+            self.check_source(meta, filepath)
+        except ValueError as e:
+            if os.path.exists(filepath):
+                self.log.error('Clean corrupted file %s' % filepath)
+                os.remove(filepath)
+            raise e
+        else:
+            self.log.info('Archive %s fetched.' % archive_gs)
